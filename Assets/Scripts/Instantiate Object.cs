934100d130fdb46473173cb8d54db56ddef02379
DavidSantiago1298 <156475324+DavidSantiago1298@users.noreply.github.com>
2025-05-14 20:40:23 -0600

    Baile Musica y botones

---
+++ Assets/Scripts/Instantiate Object.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+    [SerializeField]
+
+    private GameObject objectToInstantiate;
+
+    public void Instantiate(Transform target)
+    {
+        Instantiate(objectToInstantiate, target.position, Quaternion.identity);
+    }
+}
