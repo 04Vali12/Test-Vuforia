8911b77b57309817c787f188564c0a03cfca08b1
DavidSantiago1298 <73907804+04Vali12@users.noreply.github.com>
2025-05-21 20:33:23 -0600

    Sonidos y ya

--- Assets/Scripts/DanceController.cs
+++ Assets/Scripts/DanceController.cs
@@ -4,17 +4,29 @@ using UnityEngine.Events;
 public class DanceController : MonoBehaviour
 {
     [SerializeField]
+    private Animator _characterAnimator;
+
+    [SerializeField]
     private UnityEvent _OnSelectDance;
 
     [SerializeField]
     private UnityEvent _onDanceSelected;
 
+    private SoundData _currentSoundData;
+
     public void ActivateSelectDance()
     {
         _OnSelectDance?.Invoke();
     }
-    public void onSelectedDance()
+    public void onSelectedDance(SoundData soundData)
     {
+        _currentSoundData = soundData;
         _onDanceSelected?.Invoke();
+    }
+
+    public void StartDance()
+    {
+        _characterAnimator.Play(_currentSoundData.animationName);
+        SoundManager.instance.PlayMusic(_currentSoundData.musicName);
     }
 }
